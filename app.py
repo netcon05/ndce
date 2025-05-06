bcf9874184cf2ffbba5f8b5eb2ec30ee028fdf75
netcon05 <support@netcon.team>
2025-05-06 16:29:41 +0300

    Reset app.py.

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import asyncio
-import pprint
-from ndce.net import get_hosts_from_subnet
-from ndce.icmp import get_accesable_hosts
 
 
 async def main():
-    a = await get_accesable_hosts(
-            get_hosts_from_subnet('10.10.4.0/24')
-        )
-    print(a)
+    pass
 
 
 if __name__ == '__main__':
